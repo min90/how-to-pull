613461d1b6672cd9e36dc2ce2910f3b565889465
Jesper Sorensen <jeminor@outlook.dk>
2019-06-25 12:06:09 +0200

    test (#4)

--- Test.java
+++ Test.java
@@ -2,6 +2,5 @@ public class Test {
     public static void main (String[] args) {
         System.out.println("HEJ MED DIG OG FARVEL");
         System.out.println("FARVEL");
-        System.out.println("FARVEL");
     }
 }
